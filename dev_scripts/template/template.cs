51e13e672ad0e42587dcac9edb7d420d15a8910e
SiriusZ <582988860@qq.com>
2021-06-16 15:36:57 +0800

    Update template.cs

--- dev_scripts/template/template.cs
+++ dev_scripts/template/template.cs
@@ -24,6 +24,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using VRage.Game.ModAPI.Ingame;
 using VRage.Game.GUI.TextPanel;
